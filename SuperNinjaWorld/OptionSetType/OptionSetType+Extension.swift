6530b941cf2d8f2df95b665a857ef481864aece1
Hector Matos <hector.matos@capitalone.com>
2016-05-03 13:07:57 -0700

    adds OptionSetType files to project

---
+++ SuperNinjaWorld/OptionSetType/OptionSetType+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  OptionSetType+Extension.swift
+//  SuperNinjaWorld
+//
+//  Created by Hector Matos on 5/3/16.
+//  Copyright © 2016 Hector Matos. All rights reserved.
+//
+
+import Foundation
